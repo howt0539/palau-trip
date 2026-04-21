016c519d84cf6b91336ca485119fb55de1943ef5
howt <howt0539@gmail.com>
2026-04-21 16:21:46 +0800

    chore(planner): include brand-logo in deploy bundle
    brand-logo.jpg is used as the quote card background; copy it into the
    temp deploy dir alongside index.html so it is available in production.

--- deploy-planner.sh
+++ deploy-planner.sh
@@ -2,5 +2,6 @@
 cd "$(dirname "$0")"
 TMPDIR=$(mktemp -d)
 cp palau-trip-planner.html "$TMPDIR/index.html"
+cp brand-logo.jpg "$TMPDIR/brand-logo.jpg"
 npx wrangler pages deploy "$TMPDIR" --project-name palau-planner --branch main
 rm -rf "$TMPDIR"
